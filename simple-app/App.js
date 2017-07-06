d182a08e0b80ec1c3589693a16a2863daf042385
lele0108 <jimmyliu@berkeley.edu>
2017-07-06 16:00:47 -0700

    two files

--- simple-app/App.js
+++ simple-app/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Utils from './Utils';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {number: Utils.addStuff(1, 2)};
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <Text>{ this.state.number }</Text>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
---
+++ simple-app/Utils.js
@@ -0,0 +1,6 @@
+const addStuff = (one, two) => {
+	console.log("Adding " + one + " + " + two);
+	return one + two;
+};
+
+export default { addStuff };
